0a85fd8f2a303122e0137bfb558fa4647701d346
Aurore List <aurore.nadege.list@gmail.com>
2020-08-21 12:31:56 +0200

    création fichier Python

---
+++ Ressources/Gamekult/scrap.py
@@ -0,0 +1,3 @@
+#https://www.gamekult.com/jeux/dernieres-sorties.html
+#<div class ="pr__game-h__mdb__details">Titre, Editeur, Deveuloper, Genre, Plateforme</div>
+#<div class ="pr__game-h__mdb__details">Date de sortie par Plateforme, prix</div>
