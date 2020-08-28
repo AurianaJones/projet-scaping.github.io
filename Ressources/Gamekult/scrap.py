557cd6dfb0361fbac6793630ea11a796d28de09b
Aurore List <aurore.nadege.list@gmail.com>
2020-08-28 09:59:27 +0200

    Début de scrap pour Gamekult & modele code hugo

--- Ressources/Gamekult/scrap.py
+++ Ressources/Gamekult/scrap.py
@@ -1,3 +1,23 @@
 #https://www.gamekult.com/jeux/dernieres-sorties.html
 #<div class ="pr__game-h__mdb__details">Titre, Editeur, Deveuloper, Genre, Plateforme</div>
 #<div class ="pr__game-h__mdb__details">Date de sortie par Plateforme, prix</div>
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+html = requests.get("https://www.gamekult.com/jeux/dernieres-sorties.html")
+html_doc = html.text
+soup = BeautifulSoup(html_doc, 'html.parser')
+#print(soup.find_all("div",class_="pr__game-h__mdb__details"))
+
+titles = soup.body.find_all("span", class_="pr__game-h__mdb__details__title gk__helpers__fat-title-m")
+
+category = soup.body.find_all("span", class_="pr__game-h__mdb__details__category")
+
+
+
+test = soup.find("span", attrs={'class': "pr__game-h__mdb__details__title gk__helpers__fat-title-m"})
+result = test.find_all("a")
+
+#qu'un seul titre
+print(result)
---
+++ Ressources/Gamekult/scrap_hugo.py
@@ -0,0 +1,60 @@
+# import libraries
+import sqlite3
+from sqlite3 import connect
+conn = connect('/Users/hugofugeray/Desktop/scrap_game.db')
+curs= conn.cursor()
+from bs4 import BeautifulSoup
+import urllib.request
+import csv
+import numpy as np
+
+curs.execute('''CREATE TABLE IF NOT EXISTS games (
+id  INTEGER PRIMARY KEY AUTOINCREMENT,
+Plateforme TEXT,
+Titre TEXT,
+Genre TEXT,
+Date_de_sortie TEXT);'''
+)
+conn.commit()
+
+
+# specify the url
+urlpage = 'https://www.gamecash.fr/prochaines-sorties.html?o=t&s=a'
+
+# query the website and return the html to the variable 'page'
+page = urllib.request.urlopen(urlpage)
+
+# parse the html using beautiful soup and store in variable 'soup'
+soup = BeautifulSoup(page, 'html.parser')
+
+# find results within table
+table = soup.find('table', attrs={'class': 'table'})
+results = table.find_all('tr')
+print('Number of results', len(results))
+
+# create and write headers to a list
+rows = []
+rows.append(['PLATEFORME', 'TITRE', 'GENRE', 'DATE DE SORTIE'])
+print(rows)
+
+
+#def insert(Titre, Genre, Plateforme, Date_de_sortie):
+
+# write each result to rows
+
+# loop over results
+for result in results:
+# find all columns per result
+
+  data = result.find_all('td')
+# check that columns have data
+  if len(data) == 0:
+    continue
+# write columns to variables
+  Plateforme = data[0].getText()
+  Titre = data[2].getText()
+  Genre= data[3].getText()
+  Date_de_sortie = data[4].getText()
+  curs.execute("INSERT INTO games (Titre, Genre, Plateforme, Date_de_sortie) VALUES (?, ?, ?, ?);", (Titre, Genre, Plateforme, Date_de_sortie))
+
+  conn.commit()
