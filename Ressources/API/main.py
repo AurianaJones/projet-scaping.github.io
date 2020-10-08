0c4bd295a8ff3aec4d8375b1d1cbb14144edba2b
Auore List <asunami.al@gmail.com>
2020-10-08 12:31:18 +0200

    mise a jour

--- Ressources/API/main.py
+++ Ressources/API/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+import pandas as pd
 import uvicorn
 import sqlite3
 import requests, json
+from fastapi.responses import JSONResponse
+
 
 conn = sqlite3.connect('scrap_game.db')
 c = conn.cursor()
@@ -16,11 +19,18 @@ async def all_game ():
 
 @app.get("/games/count")
 async def count_platform():
-    c.execute("SELECT platform, COUNT(platform) FROM release_details GROUP BY platform;")
-    count = c.fetchall()
-    count = dict(count)
+    df = pd.read_sql_query("SELECT platform, COUNT(platform) FROM release_details GROUP BY platform;", conn)
+    df = df.rename(columns={'COUNT(platform)': 'pourcent', "platform" : "name"})
+    d = df.to_dict('records')
+    headers = {"Access-Control-Allow-Origin":"*"}
     conn.commit()
-    header = {'Access-Control-Allow-Origin':"*"}
-    json_count = json.dumps(count)
-    return json_count
-
+    return JSONResponse(content=d, headers=headers) 
+    
+@app.get("/games/type")
+async def diferents_types():
+    df = pd.read_sql_query("SELECT type, COUNT(type) FROM games GROUP BY type;", conn)
+    df = df.rename(columns={'COUNT(type)': 'nombres', "type" : "genre"})
+    d = df.to_dict('records')
+    headers = {"Access-Control-Allow-Origin":"*"}
+    conn.commit()
+    return JSONResponse(content=d, headers=headers) 
